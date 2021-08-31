5c5d3afb2ced7e60033f992050a7aa4b27d98c16
vivek-011299 <81247205+vivek-011299@users.noreply.github.com>
2021-08-31 13:11:35 +0530

    Create Check for balanced Binary Tree.cpp

---
+++ Check for balanced Binary Tree.cpp
@@ -0,0 +1,56 @@
+/*
+*
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+*
+*/
+
+
+Solution :-
+  
+  bool isBalanced(TreeNode* root) {
+        int x= check(root);
+        if(x==-1)
+        {
+            return false;
+        }
+        return true;
+    }
+    int check(TreeNode *root)
+    {
+        if(root==NULL)
+            return 0;
+        int x=check(root->left);
+        int y=check(root->right);
+        if(x==-1 || y==-1)
+            return -1;
+        if(abs(x-y)>1)
+            return -1;
+        return max(x,y)+1;
+    }
